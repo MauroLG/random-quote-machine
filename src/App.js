ddb6752af78dd6ed04b9c3ddb22a3bf24c495a1c
Mauro <maurogheco@gmail.com>
2020-07-06 17:11:32 -0300

    Agregado prototipo de array con quotes y generada la funcion random

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import './App.css';
 
+const arrQuotes = [
+    ['quote1', 'author1'],
+    ['quote2', 'author2'],
+    ['quote3', 'author3']
+]
+
+//const randomNum = Math.floor(Math.random() * arrQuotes.length);
+
+function getRandomNum () {
+    return Math.floor(Math.random() * arrQuotes.length);
+}
+
+
 class App extends React.Component {
     constructor(props){
         super(props);
@@ -13,9 +26,10 @@ class App extends React.Component {
     }
 
     handleClick(){
+        const randomNum = getRandomNum();
         this.setState({
-            quote: 'Ahora soy una quote',
-            author: 'Yo soy un author',
+            quote: arrQuotes[randomNum][0],
+            author: arrQuotes[randomNum][1],
         })
     }
 
