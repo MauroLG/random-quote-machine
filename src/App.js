ae762c87770274e0acd583661721cdb7d3676edd
Mauro <maurogheco@gmail.com>
2020-07-23 01:47:37 -0300

    Fixed minor things

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 const arrQuotes = [
-    ['\"My favorite things in life don’t cost any money. It’s really clear that the most precious resource we all have is time.\"', 'Steve Jobs'],
-    ['\"The Internet is becoming the town square for the global village of tomorrow.\"', 'Bill Gates'],
-    ['\"Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.\"', 'Linus Torvalds'],
-    ['\"The biggest risk is not taking any risk... In a world that changing really quickly, the only strategy that is guaranteed to fail is not taking risks.\"', 'Mark Zuckerberg'],
-    ['\"The most technologically efficient machine that man has ever invented is the book.\"', 'Northrop Frye'],
-    ['\"Just because something doesn’t do what you planned it to do doesn’t mean it’s useless.\"', 'Thomas Edison'],
-    ['\"The human spirit must prevail over technology.\"', 'Albert Einstein'],
-    ['\"It’s supposed to be automatic, but actually you have to push this button.\"', 'John Brunner']
+    ['"My favorite things in life don’t cost any money. It’s really clear that the most precious resource we all have is time."', 'Steve Jobs'],
+    ['"The Internet is becoming the town square for the global village of tomorrow."', 'Bill Gates'],
+    ['"Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program."', 'Linus Torvalds'],
+    ['"The biggest risk is not taking any risk... In a world that changing really quickly, the only strategy that is guaranteed to fail is not taking risks."', 'Mark Zuckerberg'],
+    ['"The most technologically efficient machine that man has ever invented is the book."', 'Northrop Frye'],
+    ['"Just because something doesn’t do what you planned it to do doesn’t mean it’s useless."', 'Thomas Edison'],
+    ['"The human spirit must prevail over technology."', 'Albert Einstein'],
+    ['"It’s supposed to be automatic, but actually you have to push this button."', 'John Brunner']
 ]
 
 const arrColors = [
@@ -25,8 +25,6 @@ const arrColors = [
     ["#BFAC80","#DAC592"],
     ["#B4C482","#C8DB91"],
 ]
-
-const docBody = document.querySelector('body');
 
 let randomNum;
 let lastRandomNum;
